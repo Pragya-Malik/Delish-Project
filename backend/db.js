d39fe850b73d43b5b9520257391e4ebcd049ded3
Pragya-Malik <malikpragya02@gmail.com>
2026-04-07 16:45:29 +0530

    edited db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "db",
-  database: "restaurant",
-  password: "postgres",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 
 module.exports = pool;
+
