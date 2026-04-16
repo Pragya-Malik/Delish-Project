fda4174d4f9e1d031161fde1993193c145983df1
Pragya-Malik <malikpragya02@gmail.com>
2026-04-16 18:39:16 +0530

    changed the image names in html file

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
 
-app.listen(5000, () => {
+app.listen(5000, "0.0.0.0", () => {
   console.log("Server running on port 5000");
 });
